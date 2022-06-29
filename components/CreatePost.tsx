e5dc160377e57c54ad4a344206ddc3b07aef8e5d
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-06-29 18:01:06 -0500

    Update CreatePost.tsx

--- components/CreatePost.tsx
+++ components/CreatePost.tsx
@@ -27,14 +27,16 @@ export const CreatePost: FC = () => {
 
   const { setValue } = useContext(UserContext);
   const submitPost = () => {
-    _submitPost({ variables: { author: 'unknown', title: getPostInput()?.title, content: getPostInput()?.content, createdAt: new Date().getTime().toString() } });
+    if (getPostInput().title.length !== 0) {
+      _submitPost({ variables: { author: 'unknown', title: getPostInput()?.title, content: getPostInput()?.content, createdAt: new Date().getTime().toString() } });
 
-    if (typeof window !== 'undefined') {
-      document.getElementById('title').innerHTML = '';
-      document.getElementById('content').innerHTML = '';
+      if (typeof window !== 'undefined') {
+        document.getElementById('title').innerHTML = '';
+        document.getElementById('content').innerHTML = '';
+      }
+
+      setValue({ reloadPostsList: true });
     }
-
-    setValue({ reloadPostsList: true });
   };
 
   const [_submitPost] = useMutation(mutation);
@@ -42,7 +44,7 @@ export const CreatePost: FC = () => {
   return (
     <>
       <div className='grid grid-rows-[max-content_max-content] bg-zwav-gray-300 p-[8px] rounded-[8px] gap-y-[6px]'>
-        <div id='title' placeholder='title' contentEditable='true' className='bg-zwav-gray-400 rounded-[6px] pl-[4px] outline-none text-white break-words overflow-y-auto'></div>
+        <div id='title' placeholder='title (required)' contentEditable='true' className='bg-zwav-gray-400 rounded-[6px] pl-[4px] outline-none text-white break-words overflow-y-auto'></div>
         <div id='content' placeholder='content' contentEditable='true' className='bg-zwav-gray-400 rounded-[6px] pl-[4px] outline-none text-white break-words overflow-y-auto p-[0_0_80px_0]'></div>
         <button onClick={() => submitPost()} className='w-[80px] rounded-[8px] transition ease-in-out bg-zwav-purple hover:bg-zwav-red duration-300'>
           <h2 className='text-white'>post</h2>
@@ -51,6 +53,3 @@ export const CreatePost: FC = () => {
     </>
   );
 };
-function getElementById(arg0: string) {
-  throw new Error('Function not implemented.');
-}
