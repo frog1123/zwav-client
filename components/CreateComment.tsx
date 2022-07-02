256a82e8ca9ab7c37e4bd883935eaa966402cb02
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-07-02 17:53:53 -0500

    add enter for submit

--- components/CreateComment.tsx
+++ components/CreateComment.tsx
@@ -22,7 +22,9 @@ export const CreateComment: FC<{ postId: string }> = props => {
     if (getCommentContent().length !== 0) {
       _submitComment({ variables: { postId: props.postId, author: 'unknown', content: getCommentContent(), createdAt: new Date().getTime().toString() } });
 
-      setValue({ reloadCommentsList: true });
+      if (data !== null) {
+        setValue({ reloadCommentsList: true });
+      }
 
       if (typeof window !== 'undefined') {
         (document.getElementById('content') as HTMLInputElement).value = '';
@@ -30,12 +32,17 @@ export const CreateComment: FC<{ postId: string }> = props => {
     }
   };
 
-  const [_submitComment] = useMutation(mutation);
+  const [_submitComment, { data }] = useMutation(mutation);
+
+  const handleKeyPress = (e: any) => {
+    if (e.which == '13') e.preventDefault();
+    if (e.key === 'Enter') submitComment();
+  };
 
   return (
     <div>
       <div className='grid grid-rows-[max-content_max-content] bg-zwav-gray-300 p-[8px] rounded-[8px] gap-y-[6px]'>
-        <TextareaAutosize id='content' placeholder='say something...' className='bg-zwav-gray-400 rounded-[6px] pl-[4px] pr-[2px] outline-none text-white no-scrollbar resize-none' />
+        <TextareaAutosize id='content' onKeyPress={e => handleKeyPress(e)} placeholder='say something...' className='bg-zwav-gray-400 rounded-[6px] pl-[4px] pr-[2px] outline-none text-white no-scrollbar resize-none' />
         <button onClick={() => submitComment()} className='w-[80px] rounded-[8px] transition ease-in-out bg-zwav-color hover:bg-zwav-color-hover duration-[0.25s]'>
           <h2 className='text-white'>comment</h2>
         </button>
--- components/CreatePost.tsx
+++ components/CreatePost.tsx
@@ -36,17 +36,23 @@ export const CreatePost: FC = () => {
         (document.getElementById('title') as HTMLInputElement).value = '';
         (document.getElementById('content') as HTMLInputElement).value = '';
       }
-
-      setValue({ reloadPostsList: true });
+      if (data !== null) {
+        setValue({ reloadPostsList: true });
+      }
     }
   };
 
-  const [_submitPost] = useMutation(mutation);
+  const [_submitPost, { data }] = useMutation(mutation);
+
+  const handleKeyPress = (e: any) => {
+    if (e.which == '13') e.preventDefault();
+    if (e.key === 'Enter') submitPost();
+  };
 
   return (
     <>
       <div className='grid grid-rows-[max-content_max-content] bg-zwav-gray-300 p-[8px] rounded-[8px] gap-y-[6px]'>
-        <TextareaAutosize id='title' placeholder='title (required)' className='bg-zwav-gray-400 rounded-[6px] pl-[4px] pr-[2px] outline-none overflow-x-hidden text-white no-scrollbar resize-none' />
+        <TextareaAutosize id='title' onKeyPress={e => handleKeyPress(e)} placeholder='title (required)' className='bg-zwav-gray-400 rounded-[6px] pl-[4px] pr-[2px] outline-none overflow-x-hidden text-white no-scrollbar resize-none' />
         <TextareaAutosize id='content' placeholder='content' className='bg-zwav-gray-400 rounded-[6px] pl-[4px] pr-[2px] outline-none text-white pb-[80px] no-scrollbar resize-none' />
         <button onClick={() => submitPost()} className='w-[80px] rounded-[8px] transition ease-in-out border-none bg-zwav-color hover:bg-zwav-color-hover duration-[0.25s]'>
           <h2 className='text-white'>post</h2>
